2755f478bf29864c7e7c72e12ff81ee7820d5d70
Arne Kiesewetter <kiesewetter.arne@t-online.de>
2013-11-13 17:31:48 +1300

    Plugin loading

---
+++ IRC/Plugins/PluginManager.cs
@@ -0,0 +1,45 @@
+﻿using Kiwana.Core.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Kiwana.Core.Plugins
+{
+    public static class PluginManager
+    {
+        private static XmlSerializer _pluginConfigSerializer = new XmlSerializer(typeof(PluginConfig));
+
+        public static List<Plugin> ScanPluginFolder(string folder)
+        {
+            List<Plugin> plugins = new List<Plugin>();
+
+            string[] pluginPaths = Directory.GetFiles(folder, "*.dll");
+
+            Console.WriteLine("Loading Plugins from " + folder + " ...");
+
+            foreach (string pluginPath in pluginPaths)
+            {
+                Plugin plugin = new Plugin();
+                plugin.Name = Regex.Match(pluginPath, @"\w+(?=.dll)", RegexOptions.IgnoreCase).Value;
+
+                Console.WriteLine("  - " + plugin.Name);
+
+                XmlReader reader = XmlReader.Create(folder + "/" + plugin.Name + "Config.xml");
+                plugin.Config = (PluginConfig)_pluginConfigSerializer.Deserialize(reader);
+
+                Assembly dll = Assembly.LoadFile(Path.GetFullPath(pluginPath));
+                plugin.Instance = Activator.CreateInstance(dll.GetType(plugin.Config.ClassName));
+
+                plugins.Add(plugin);
+            }
+
+            Console.WriteLine("Done!");
+
+            return plugins;
+        }
+    }
+}
